fd9a8e4975eb4335197e35a87f47ed06cabbd477
Evgenii Semenov <eo.semenoff@gmail.com>
2021-08-17 15:34:21 +0300

    credit card numbers added to datahelper

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,5 +1,7 @@
 package ru.netology.web.test;
 
+import lombok.Data;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.LoginPageV1;
@@ -7,37 +9,25 @@ import ru.netology.web.page.LoginPageV2;
 import ru.netology.web.page.LoginPageV3;
 
 import static com.codeborne.selenide.Selenide.open;
+@Data
 
 class MoneyTransferTest {
-    @Test
-    void shouldTransferMoneyBetweenOwnCardsV1() {
-      open("http://localhost:9999");
-      var loginPage = new LoginPageV1();
-//    var loginPage = open("http://localhost:9999", LoginPageV1.class);
-      var authInfo = DataHelper.getAuthInfo();
-      var verificationPage = loginPage.validLogin(authInfo);
-      var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-      verificationPage.validVerify(verificationCode);
-    }
 
-  @Test
-  void shouldTransferMoneyBetweenOwnCardsV2() {
+  @BeforeEach
+  void browserSetup() {
     open("http://localhost:9999");
-    var loginPage = new LoginPageV2();
-//    var loginPage = open("http://localhost:9999", LoginPageV2.class);
-    var authInfo = DataHelper.getAuthInfo();
-    var verificationPage = loginPage.validLogin(authInfo);
-    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
+    ;
   }
 
   @Test
-  void shouldTransferMoneyBetweenOwnCardsV3() {
-    var loginPage = open("http://localhost:9999", LoginPageV3.class);
+  void shouldTransferMoneyBetweenOwnCards() {
+    var loginPage = new LoginPageV2();
     var authInfo = DataHelper.getAuthInfo();
     var verificationPage = loginPage.validLogin(authInfo);
     var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
     verificationPage.validVerify(verificationCode);
+
   }
 }
+
 
